693a5c649ea6132a066f3bb9ac7999640266f85f
Julien Roland <juroland@gmail.com>
2017-02-09 22:37:20 +0100

    Add functions and types to be implemented. See : https://en.wikipedia.org/wiki/Test_functions_for_optimization

---
+++ testfunc/testfunc.go
@@ -0,0 +1,31 @@
+package testfunc
+
+type SearchDomain struct {
+	UpperBound []float64
+	LowerBound []float64
+}
+
+type GlobalMinimum struct {
+	X     []float64
+	Value float64
+}
+
+func Rastrigin(x []float64) (z float64)        { return }
+func Ackley(x, y, float64) (z float64)         { return }
+func Sphere(x []float64) (z float64)           { return }
+func Rosenbrock(x []float64) (z float64)       { return }
+func Beale(x, y, float64) (z float64)          { return }
+func GoldsteinPrice(x, y, float64) (z float64) { return }
+func Booth(x, y, float64) (z float64)          { return }
+func BukinN6(x, y, float64) (z float64)        { return }
+func Matyas(x, y, float64) (z float64)         { return }
+func LeviN13(x, y, float64) (z float64)        { return }
+func ThreeHumpCamel(x, y, float64) (z float64) { return }
+func Easom(x, y, float64) (z float64)          { return }
+func CrossInTray(x, y, float64) (z float64)    { return }
+func Eggholder(x, y, float64) (z float64)      { return }
+func HolderTable(x, y, float64) (z float64)    { return }
+func McCormick(x, y, float64) (z float64)      { return }
+func SchafferN2(x, y, float64) (z float64)     { return }
+func SchafferN4(x, y, float64) (z float64)     { return }
+func StyblinskiTang(x []float64) (z float64)   { return }
